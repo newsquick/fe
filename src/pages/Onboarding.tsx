1812583d1aba228b253e40647b6abd1e236728e7
saseungg <yeon14766@gmail.com>
2024-04-02 21:56:25 +0900

    feat: carousel 구현, 스타일 커스텀

--- src/pages/Onboarding.tsx
+++ src/pages/Onboarding.tsx
@@ -1,5 +1,7 @@
 import NavigateButton from 'components/common/NavigateButton';
-import Slider from 'components/home/Slider';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
 
 const slides = [
   {
@@ -26,24 +28,34 @@ const slides = [
   },
 ];
 
-// TODO: 무한 슬라이더 구현
 const Onboarding = () => {
-  const options = {
-    slideWidth: 452,
-    slideCount: 3,
-    showDots: true,
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dotsClass: 'dots-custom',
   };
 
   return (
-    <div className="mx-auto h-screen w-full px-6 py-9">
-      <div className="h-[calc(100%-55px)] w-full">
-        <Slider {...options}>
+    <div className="mx-auto flex h-full w-full flex-col justify-between px-6 py-9">
+      <div className="h-full w-full">
+        <Slider {...settings}>
           {slides.map(({ image, description }, index) => (
-            <div key={index} className="flex flex-col items-center justify-center">
-              <img src={image} className="w-[452px]" />
-              <div className="flex flex-col gap-[10px]">
+            <div
+              key={index}
+              className="flex h-full w-full cursor-pointer flex-col items-center justify-center"
+            >
+              <div
+                className="bg-bottom-4 h-[calc(100%-80px)] w-full bg-contain bg-no-repeat"
+                style={{
+                  backgroundImage: `url(${image})`,
+                }}
+              />
+              <div className="flex h-20 flex-col justify-between">
                 <p className="text-center  text-[23px] font-bold text-gray1000">{description.title}</p>
-                <p className="trackimg-[-0.5px]  whitespace-pre-line text-center text-[14px] text-gray800">
+                <p className="trackimg-[-0.5px] whitespace-pre-line text-center text-[14px] text-gray800">
                   {description.subTitle}
                 </p>
               </div>
