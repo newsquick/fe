65c112cea83372bb597fab01a843af4f73f9a81f
saseungg <yeon14766@gmail.com>
2024-04-09 01:04:42 +0900

    refactor: home 레이아웃 높이값 동적할당으로 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,21 @@
 import Logo from 'assets/images/logo.svg?react';
+import HomeHeader from 'components/common/HomeHeader';
 import NavigateButton from 'components/common/NavigateButton';
+
+const HEADER_HEIGHT = 64;
+const BUTTON_HEIGHT = 55;
+const MAIN_HEIGHT = `calc(100% - ${HEADER_HEIGHT + BUTTON_HEIGHT}px)`;
 
 const Home = () => {
   return (
-    <div className="relative h-screen w-full bg-[url('/assets/bg_home.png')] bg-cover px-6 py-9">
-      <div className="flex h-[calc(100%-55px)] flex-col items-center justify-center gap-3">
+    <div className="relative h-screen w-full bg-[url('/assets/bg_home.png')] bg-cover px-6 pb-9">
+      <HomeHeader />
+      <div
+        className="flex flex-col items-center justify-center gap-3"
+        style={{
+          height: MAIN_HEIGHT,
+        }}
+      >
         <Logo />
         <p className="text-[18px] font-medium text-gray1000">개인 맞춤형 축사 생성 서비스</p>
       </div>
