c0e390e52bb934afe6c4128e520b5133e93c8d4d
saseungg <yeon14766@gmail.com>
2024-04-10 02:14:48 +0900

    refactor: 캐러셀 슬라이드 변수 처리

--- src/components/onboarding/Carousel.tsx
+++ src/components/onboarding/Carousel.tsx
@@ -1,31 +1,7 @@
+import { ONBOARDING_SLIDES } from 'constants/index';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
-
-const SLIDES = [
-  {
-    image: '/assets/onboarding_1.webp',
-    description: {
-      title: '막막한 축사 준비를 손쉽게',
-      subTitle: '소중한 사람의 축복을 위해 진심을 전할 수 있도록\nAI가 맞춤형 축사 문장을 바로 생성해 줘요.',
-    },
-  },
-
-  {
-    image: '/assets/onboarding_2.webp',
-    description: {
-      title: '축사 주의사항도 미리 살펴보세요',
-      subTitle: '축사가 막막하게 느껴진다면, 주의사항을\n미리 살펴보고 걱정을 덜어보세요.',
-    },
-  },
-  {
-    image: '/assets/onboarding_3.webp',
-    description: {
-      title: '뻔하지 않은, 진심을 담은 축사',
-      subTitle: '내 상황에 맞춰, 단 하나밖에 없는\n축사 문장을 만들어 드려요.',
-    },
-  },
-];
 
 const SLIDER_SETTINGS = {
   dots: true,
@@ -43,7 +19,7 @@ const Carousel = () => {
   return (
     <div className="h-full w-full pb-16">
       <Slider {...SLIDER_SETTINGS}>
-        {SLIDES.map(({ image, description }, index) => (
+        {ONBOARDING_SLIDES.map(({ image, description }, index) => (
           <div key={index} className="flex h-full w-full cursor-pointer flex-col items-center justify-center">
             <div
               className="bg-bottom-4 w-full bg-contain bg-no-repeat"
