ae598f408f992c1a9b86c80f6de1e762d8064ed5
saseungg <yeon14766@gmail.com>
2024-04-10 19:09:27 +0900

    refactor: header -> resultHeader로 변경

---
+++ src/components/result/resultSection/ResultHeader.tsx
@@ -0,0 +1,16 @@
+import ChevronIcon from 'assets/images/chevron-left.svg?react';
+import ShareButton from 'components/common/ShareButton';
+import { Link } from 'react-router-dom';
+
+export default function ResultHeader() {
+  return (
+    <div className="flex h-16 w-full items-center justify-between">
+      <Link to={'/question'}>
+        <button className="flex h-6 w-6 items-center justify-start" data-ga="header_back">
+          <ChevronIcon />
+        </button>
+      </Link>
+      <ShareButton />
+    </div>
+  );
+}
--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,7 +1,7 @@
 import EditIcon from 'assets/images/edit.svg?react';
-import Header from 'components/common/Header';
 import Loading from 'components/common/Loading';
 import CopyToClipboardButton from 'components/result/resultSection/ClipboardButton';
+import ResultHeader from 'components/result/resultSection/ResultHeader';
 import ResultRetryButton from 'components/result/resultSection/ResultRetryButton';
 import ResultTitle from 'components/result/resultSection/ResultTitle';
 import SaveImageButton from 'components/result/resultSection/SaveImageButton';
@@ -41,7 +41,7 @@ const Result = () => {
       ) : (
         <>
           <div className="flex flex-col items-center bg-gradient bg-cover px-6">
-            <Header />
+            <ResultHeader />
             <ResultTitle name={name} />
             <div className="mb-5 w-full rounded-[10px] border border-white border-opacity-60 bg-white bg-opacity-50 bg-clip-padding px-[26px] pb-[26px] pt-[29px] backdrop-blur-sm backdrop-filter">
               <span className="whitespace-pre-line text-[15px] leading-[170%] tracking-[-0.6px] text-gray800">
