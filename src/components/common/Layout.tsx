0c9d70a370fc942c3b5ef81d4d98756f4de08489
saseungg <yeon14766@gmail.com>
2024-04-12 18:09:05 +0900

    feat: notfound 페이지 구현

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,6 +1,6 @@
 const Layout = ({ children }: ChildrenProps) => {
   return (
-    <div className="bg-slate-100] mx-auto w-full">
+    <div className="mx-auto w-full bg-slate-100">
       <div className="mx-auto h-screen w-full max-w-[500px] bg-white shadow-xl">{children}</div>
     </div>
   );
