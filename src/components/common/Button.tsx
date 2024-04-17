6bb098c879f02906882330fbf0884951cb34c093
saseungg <yeon14766@gmail.com>
2024-04-17 17:46:45 +0900

    feat: button type 받을 수있도록 rest 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,13 +1,14 @@
-type Props = {
+type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   children: string;
   disabled?: any;
   ga?: string;
   onClick?: any;
 };
 
-const Button = ({ children, disabled, ga, onClick }: Props) => {
+const Button = ({ children, disabled, ga, onClick, ...rest }: Props) => {
   return (
     <button
+      {...rest}
       className="flex h-[55px] w-full items-center justify-center rounded-[10px] bg-gray900 text-[17px] font-bold text-white disabled:bg-gray-400"
       disabled={disabled}
       data-ga={ga}
