fa44b087cd9cc77150144681b9b879b64b5685b1
saseungg <yeon14766@gmail.com>
2024-04-18 00:07:42 +0900

    refactor: Question 페이지 GenericFormProvider 반영

--- src/pages/Question.tsx
+++ src/pages/Question.tsx
@@ -3,27 +3,14 @@ import Loading from 'components/common/Loading';
 import Layout from 'components/question/Layout';
 import ProgressBar from 'components/question/ProgressBar';
 import QuestionFunnel from 'components/question/QuestionFunnel';
+import { QUESTION_STEPS } from 'constants/index';
+import { GenericFormProvider } from 'contexts/GenericFormContex';
 import usePostMessage from 'hooks/apis/usePostMessage';
 import { useFunnel } from 'hooks/useFunnel';
-import { FormProvider, useForm } from 'react-hook-form';
-
-export const STEPS = [
-  '사용자이름',
-  '대상자이름',
-  '대상유형',
-  '관계',
-  '축사시간',
-  '말투',
-  '컨셉',
-  '이야기',
-  '마지막할말',
-];
 
 const Question = () => {
-  const methods = useForm<AnswerData>();
   const { isLoading, handlePost } = usePostMessage();
-  const { Funnel, Step, prevStep, nextStep, stepNum } = useFunnel(STEPS);
-  console.log(stepNum);
+  const { Funnel, Step, prevStep, nextStep, stepNum } = useFunnel(QUESTION_STEPS);
 
   return (
     <>
@@ -33,11 +20,9 @@ const Question = () => {
         <Layout>
           <Header onPrev={prevStep} />
           <ProgressBar currentStep={stepNum} />
-          <FormProvider {...methods}>
-            <form className="h-full" onSubmit={methods.handleSubmit(handlePost)}>
-              <QuestionFunnel Funnel={Funnel} Step={Step} nextStep={nextStep} />
-            </form>
-          </FormProvider>
+          <GenericFormProvider onSubmit={handlePost}>
+            <QuestionFunnel Funnel={Funnel} Step={Step} nextStep={nextStep} />
+          </GenericFormProvider>
         </Layout>
       )}
     </>
