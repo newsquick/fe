ad75a4c29c492a06a60277fb67894e3ed58f0fb5
saseungg <yeon14766@gmail.com>
2024-04-18 00:04:38 +0900

    feat: formContext와 Provider를 감싸주는 GenericFormContext 구현

---
+++ src/contexts/GenericFormContex.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { FormProvider, SubmitHandler, useForm, useFormContext } from 'react-hook-form';
+
+type Props = {
+  children: React.ReactNode;
+  onSubmit: SubmitHandler<AnswerData>;
+};
+
+const defaultValues: AnswerData = {
+  userName: '',
+  targetName: '',
+  targetType: '',
+  relationship: '',
+  minute: '',
+  speechType: '',
+  concept: '',
+  story: '',
+  lastComment: '',
+  isRenew: false,
+};
+
+export const GenericFormProvider = ({ children, onSubmit }: Props) => {
+  const methods = useForm<AnswerData>({
+    defaultValues: defaultValues,
+    mode: 'onChange',
+    shouldFocusError: false,
+  });
+
+  return (
+    <FormProvider {...methods}>
+      <form className="h-[calc(100%-84px)]" onSubmit={methods.handleSubmit(onSubmit)}>
+        {children}
+      </form>
+    </FormProvider>
+  );
+};
+
+export const useGenericFormContext = () => {
+  return useFormContext<AnswerData>();
+};
