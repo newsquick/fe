15686928a0cf3b419b10392228948ec6b55603e0
saseungg <yeon14766@gmail.com>
2024-04-18 00:06:29 +0900

    feat: 마지막할말, 스토리 step 구현

--- src/components/question/step/LastCommentStep.tsx
+++ src/components/question/step/LastCommentStep.tsx
@@ -1,37 +1,46 @@
 import Button from 'components/common/Button';
-import { useFormContext } from 'react-hook-form';
+import Textarea from 'components/common/Textarea';
+import { useGenericFormContext } from 'contexts/GenericFormContex';
+import { useWatch } from 'react-hook-form';
 
 import QuestionTitle from '../QuestionTitle';
 import Layout from './Layout';
 
 const LastCommentStep = () => {
-  const { register } = useFormContext();
+  const {
+    register,
+    control,
+    formState: { isValid, errors },
+  } = useGenericFormContext();
+
+  const lastComment = useWatch({
+    control,
+    name: 'lastComment',
+  });
 
   return (
-    <>
-      <Layout>
-        <div>
-          <QuestionTitle text={'축사에서 마지막으로 하고\n싶은 말이 있다면 알려 주세요.'} />
-          <label className="relative flex flex-col gap-3" htmlFor="textarea">
-            <div className="h-[200px] w-full rounded-[5px] bg-gray100 pt-[15px]">
-              <textarea
-                className="h-[159px] w-full resize-none bg-gray100 pl-[16px] pr-[35px]  text-[15px] leading-[150%] tracking-[-0.5px] text-gray900 placeholder-gray500 focus:outline-none"
-                id="textarea"
-                maxLength={400}
-                placeholder="ex. 행복하게 잘 살아라!"
-                {...register('lastComment')}
-              />
-              <span className="absolute bottom-2.5 right-3.5 mt-[30px]  text-[14px] tracking-[-0.3px] text-gray400">
-                400자
-              </span>
-            </div>
-          </label>
-        </div>
-        <Button type="submit" ga="question_9th">
-          내 축사 확인하기
-        </Button>
-      </Layout>
-    </>
+    <Layout>
+      <div>
+        <QuestionTitle text={'축사에서 마지막으로 하고\n싶은 말이 있다면 알려 주세요.'} />
+        <Textarea
+          register={register('lastComment', {
+            required: '마지막 할 말은 필수 입력입니다.',
+            minLength: {
+              value: 3,
+              message: '3글자 이상 입력해주세요.',
+            },
+          })}
+          error={errors.lastComment?.message}
+          counter={lastComment.length}
+          id="lastComment"
+          placeholder="ex. 행복하게 잘 살아라!"
+          maxLength={400}
+        />
+      </div>
+      <Button type="submit" disabled={!isValid} ga="question_9th">
+        내 축사 확인하기
+      </Button>
+    </Layout>
   );
 };
 
--- src/components/question/step/StoryStep.tsx
+++ src/components/question/step/StoryStep.tsx
@@ -1,5 +1,7 @@
 import Button from 'components/common/Button';
-import { useFormContext } from 'react-hook-form';
+import Textarea from 'components/common/Textarea';
+import { useGenericFormContext } from 'contexts/GenericFormContex';
+import { useWatch } from 'react-hook-form';
 
 import QuestionTitle from '../QuestionTitle';
 import Layout from './Layout';
@@ -9,28 +11,37 @@ type Props = {
 };
 
 const StoryStep = ({ nextStep }: Props) => {
-  const { register } = useFormContext();
+  const {
+    register,
+    control,
+    formState: { isValid, errors },
+  } = useGenericFormContext();
+
+  const story = useWatch({
+    control,
+    name: 'story',
+  });
 
   return (
     <Layout>
       <div>
         <QuestionTitle text={'축사 중간에 들어갈 재밌는\n에피소드가 있다면 알려 주세요.'} />
-        <label className="relative flex flex-col gap-3" htmlFor="textarea">
-          <div className="h-[200px] w-full rounded-[5px] bg-gray100 pt-[15px]">
-            <textarea
-              className="h-[159px] w-full resize-none bg-gray100 pl-[16px] pr-[35px]  text-[15px] leading-[150%] tracking-[-0.5px] text-gray900 placeholder-gray500 focus:outline-none"
-              id="textarea"
-              maxLength={400}
-              placeholder="ex. 매일같이 학교 앞에서 떡볶이 사먹으며 친하게 지냈었다."
-              {...register('story')}
-            />
-            <span className="absolute bottom-2.5 right-3.5 mt-[30px]  text-[14px] tracking-[-0.3px] text-gray400">
-              0/400자
-            </span>
-          </div>
-        </label>
+        <Textarea
+          register={register('story', {
+            required: '이야기는 필수 입력입니다.',
+            minLength: {
+              value: 3,
+              message: '3글자 이상 입력해주세요.',
+            },
+          })}
+          error={errors.story?.message}
+          counter={story.length}
+          id="lastComment"
+          placeholder="ex. 매일같이 학교 앞에서 떡볶이 사먹으며 친하게 지냈었지."
+          maxLength={400}
+        />
       </div>
-      <Button onClick={nextStep} ga="question_8th">
+      <Button type="button" disabled={!isValid} onClick={nextStep} ga="question_8th">
         다음
       </Button>
     </Layout>
@@ -38,14 +49,3 @@ const StoryStep = ({ nextStep }: Props) => {
 };
 
 export default StoryStep;
-
-/* 
-              {value.length}/400자
-            </span>
-          </div>
-        </label>
-      </div>
-      <Button disabled={value.length === 0} onClick={nextStep} ga="question_8th">
-        다음
-
-*/
