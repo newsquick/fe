67fe6f67d1700232c60a80c8a4e0cf4d47334f04
saseungg <yeon14766@gmail.com>
2024-04-22 23:36:43 +0900

    feat: 공유 버튼 토스트 처리

--- src/components/common/ShareButton.tsx
+++ src/components/common/ShareButton.tsx
@@ -1,10 +1,14 @@
 import ShareIcon from 'assets/svg/share.svg?react';
+import Toast from 'components/result/Toast';
+import useToast from 'hooks/useToast';
 
 type Props = {
   url: string;
 };
 
 const ShareButton = ({ url }: Props) => {
+  const { isShow, handleShowToast } = useToast(3000);
+
   const handleShare = () => {
     if (navigator.share) {
       navigator.share({
@@ -12,16 +16,19 @@ const ShareButton = ({ url }: Props) => {
         url: url,
       });
     } else if (navigator.clipboard) {
-      navigator.clipboard.writeText(url).then(() => alert('링크가 클립보드에 복사되었습니다.'));
+      handleShowToast();
     } else {
       alert('공유하기가 지원되지 않는 환경 입니다.');
     }
   };
 
   return (
-    <button className="flex h-6 w-6 items-center justify-end" onClick={handleShare} data-ga="header_share">
-      <ShareIcon />
-    </button>
+    <>
+      <button className="flex items-center justify-end w-6 h-6" onClick={handleShare} data-ga="header_share">
+        <ShareIcon />
+      </button>
+      <Toast isShow={isShow} message="링크를 성공적으로 복사했어요!" />
+    </>
   );
 };
 
