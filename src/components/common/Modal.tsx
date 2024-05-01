4abc31151da24b32166824450941b11638bf1298
saseungg <yeon14766@gmail.com>
2024-05-01 20:18:43 +0900

    style: 주석 제거

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -62,18 +62,3 @@ Modal.SubTitle = SubTitle;
 Modal.ActionButton = ActionButton;
 
 export default Modal;
-
-/*   <div className="fixed left-2/4 top-2/4 z-50 translate-x-[-50%] translate-y-[-50%]">
-            <div className="w-[285px] rounded-[10px] bg-white px-[20px] pb-[19px] pt-[49px]">
-              <button className="absolute right-4 top-4" onClick={onClose}>
-                <CloseIcon width={10} height={10} />
-              </button>
-              {children}
-              <button
-                className="mt-[32px] flex h-[48px] w-full items-center justify-center rounded-[10px] bg-gray900 font-semibold text-white"
-                onClick={onConfirm}
-              >
-                {buttonText}
-              </button>
-            </div>
-          </div> */
