d5484831f9e6ebf9b6a7402005bc8cb4f66a9383
saseungg <yeon14766@gmail.com>
2024-05-01 20:49:53 +0900

    fix: 브라우저 환경 확인 후, 카카오일 경우 support false를 반환하도록 구현

--- src/hooks/useSaveImage.ts
+++ src/hooks/useSaveImage.ts
@@ -6,16 +6,18 @@ const useSaveImage = () => {
   const [isSupported, setIsSupported] = useState(true);
 
   const handleSaveImage = async () => {
-    if (!targetRef.current) return;
+    if (targetRef.current && navigator.userAgent.indexOf('KAKAO') === -1) {
+      try {
+        const canvas = await html2canvas(targetRef.current, { useCORS: true });
+        const downloadLink = document.createElement('a');
 
-    try {
-      const canvas = await html2canvas(targetRef.current, { useCORS: true });
-      const downloadLink = document.createElement('a');
-
-      downloadLink.href = canvas.toDataURL('image/jpeg');
-      downloadLink.download = 'bloom.jpg';
-      downloadLink.click();
-    } catch (error) {
+        downloadLink.href = canvas.toDataURL('image/jpeg');
+        downloadLink.download = 'bloom.jpg';
+        downloadLink.click();
+      } catch (error) {
+        alert('이미지를 다운받을 수 없습니다.');
+      }
+    } else {
       setIsSupported(false);
     }
   };
